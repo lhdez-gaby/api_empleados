dc6f733b024d5ea6e882002ae1dc111c5acaaf14
Gabriela Lopez <lhdez.gaby@gmail.com>
2025-01-16 16:45:43 -0600

    add employee basic service

---
+++ API_empleados/Services/EmployeeService.cs
@@ -0,0 +1,26 @@
+using API_empleados.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace API_empleados.Services
+{
+    public class EmployeeService
+    {
+        private readonly IMongoCollection<Employee> _employees;
+        public EmployeeService(IOptions<DatabaseSettings> databaseSettings)
+        {
+            var mongoClient = new MongoClient(databaseSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(databaseSettings.Value.DatabaseName);
+            _employees = mongoDatabase.GetCollection<Employee>(databaseSettings.Value.CollectionName);
+        }
+
+        public async Task<List<Employee>> GetEmployees()=>
+        await _employees.Find(employee => true).ToListAsync();
+
+        public async Task<Employee> CreateEmployee(Employee employee)
+        {
+            await _employees.InsertOneAsync(employee);
+            return employee;
+        }
+    }
+}
--- API_empleados/obj/Debug/net8.0/API_empleados.AssemblyInfo.cs
+++ API_empleados/obj/Debug/net8.0/API_empleados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_empleados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ead8fc031c2e33ac46beb1d7f0f09b40051983a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c77226d5f6f109f507ae36b5c2a37f11677c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_empleados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_empleados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
